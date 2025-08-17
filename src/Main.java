c25d25272d57adfd970a3ce75cdb314b50d84d0d
NathanW2023 <nathan.wu2@students.mq.edu.au>
2025-08-18 08:59:15 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,13 @@ public class Main extends JFrame {
       window.run();
     }
 
+    class Cell{
+
+    }
+
+    class Grid{
+      
+    }
     class Canvas extends JPanel {
       public Canvas() {
         setPreferredSize(new Dimension(720, 720));
@@ -18,8 +25,16 @@ public class Main extends JFrame {
       public void paint(Graphics g) {
 	g.setColor(java.awt.Color.BLACK);
 	g.drawRect(10, 10, 700, 700);
+      for (int row = 0; row < 20; row++) {
+        for (int col = 0; col < 20; col++) {
+          int x = 10 + (col * 35);
+          int y = 10 + (row * 35);
+          g.drawRect(x, y, 35, 35);
+          }
+        }
       }
     }
+
 
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
